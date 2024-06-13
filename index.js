8393fa5b7da411f191cb55436de6e09ec494595c
Yash <yash.verma@tailboost.ai>
2024-06-13 20:06:02 +0530

    just a commit

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.use(
 
 app.use(express.json({ limit: "16kb" }));
 
-app.listen(4000, () => {
-  console.log(`Server is running at port : http://localhost:4000 `);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at port : http://localhost:${process.env.PORT} `);
 });
 
 const __dirname = path.resolve("./"); // Get the absolute path of the current directory
@@ -40,6 +40,7 @@ app.use(
 );
 
 app.get("/health", (req, res) => {
+  // never tough this 
   res.json({ status: "healthy" });
 });
 
